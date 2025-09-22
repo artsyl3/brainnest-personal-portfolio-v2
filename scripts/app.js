632c436868806ccfa1f2585ad6b0a53d70137fcf
Hossein Ghanbari <118113234+artsyl3@users.noreply.github.com>
2025-09-22 13:30:43 +0200

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      entry.target.classList.add("show");
+      entry.target.classList.add("shooooow");
     } else {
       entry.target.classList.remove("shoow");
     }
